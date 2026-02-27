23c5deaeeee7b9badf344530bb066ed625e975d9
1TheCrazy <richardbadouin@gmail.com>
2026-02-27 17:19:01 +0100

    slight program refactor

--- include/objects/Program.h
+++ include/objects/Program.h
@@ -2,13 +2,14 @@
 #include <objects/ELF.h>
 
 class Program{
-    // Fixed size array for now
-    uint8_t RAM[200000];
-    uint64_t REG[33];
-    uint64_t program_counter;
+    public:
+        // Fixed size array for now
+        uint8_t RAM[200000];
+        uint64_t REG[33];
+        uint64_t program_counter;
 
-    // Step by one simulated "clock cycle"
-    void step();
-    // Setup: load the program into RAM, ...
-    void init(const ELF& elf, const std::vector<uint8_t>& bytes);
+        // Step by one simulated "clock cycle"
+        void step();
+        // Setup: load the program into RAM, ...
+        Program(const ELF& elf, const std::vector<uint8_t>& bytes);
 };
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #include <util/FileUtil.h>
 #include <util/Log.h>
 #include <objects/ELF.h>
+#include <objects/Program.h>
 
 // RAM doesn't do anything useful for now
 vector<uint8_t> RAM;
@@ -11,17 +12,8 @@ vector<uint8_t> RAM;
 int main(){
     const auto bytes = read_file_bytes("./hello_world");
     ELF elf = ELF(bytes);
+    Program program = Program(elf, bytes);
 
-    for(int i = 0; i < elf.program_headers.size(); i++){
-        auto header = elf.program_headers[i];
-
-        log("---------");
-        log("MemSize: " + to_string(header.p_memsz));
-        log("VirtualAddr: " + to_string(header.p_vaddr));
-        log("FileSize: " + to_string(header.p_filesz));
-    }
-
-    log(to_string(elf.elf_header.e_phentsize));
     return 0;
 }
 
